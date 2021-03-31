a74ae3ee1d215d9075ee2fbb183b7a26f3d1991c
Luiz Fernando <lfnando.nando@gmail.com>
2021-03-31 00:43:42 -0300

    31/03/2021

--- Best-Me-App/index.js
+++ Best-Me-App/index.js
@@ -1,5 +1,10 @@
 // process.stdout.write("Alguma coisa \n") // vai escrever algo de saída, parecido ao console.log()
 
+
+
+// PARA EXECUTAR: 
+// npm index
+
 const questions = [
   "O que aprendi hoje?",
   "O que me deixou aborrecido? E o que eu poderia fazer para melhorar",
@@ -8,12 +13,36 @@ const questions = [
 ]
 
 const ask = (index = 0) => {
-  return process.stdout.write(questions[index] + "\n\n\n") // printa apenas a frase na posicao [0]
+  return process.stdout.write("\n\n" + questions[index] + " > ") // printa apenas a frase na posicao [0]
 }
 
 ask()
 
+const answers = []
 process.stdin.on("data", data => {
-  process.stdout.write(data.toString().trim() + '\n') //pega o input do usuário e printa
-  process.exit()
+  // process.stdout.write(data.toString().trim() + '\n') / /pega o input do usuário e printa
+  answers.push(data.toString().trim())
+  if (answers.length < questions.length) {
+    ask(answers.length)
+  } else {
+    process.exit()
+  }
+})
+
+process.on('exit', () => {
+  console.log(`
+    Bacana!
+    
+    O que você aprendeu hoje foi: 
+    ${answers[0]}
+
+    O que te aborreceu e você poderia melhorar foi:
+    ${answers[1]}
+
+    O que te deixou feliz hoje:
+    ${answers[2]}
+
+    Você ajudou ${answers[3]} pessoas hoje!!
+
+    Volte amanhã para novas reflexões`)
 })
